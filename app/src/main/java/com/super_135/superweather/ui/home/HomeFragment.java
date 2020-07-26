2ce4dee9e8a31716a14481e266a816c8c7766dcb
Super-135 <super05061982@gmail.com>
2020-07-26 09:03:18 +0300

    Version_0_1

--- app/src/main/java/com/super_135/superweather/ui/home/HomeFragment.java
+++ app/src/main/java/com/super_135/superweather/ui/home/HomeFragment.java
@@ -6,13 +6,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.super_135.superweather.R;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-
-import com.super_135.superweather.R;
 
 public class HomeFragment extends Fragment {
 
